ef29f23e371a07f341fa970f5313bc674437057e
Jay <soon.nerd@gmail.com>
2020-10-01 10:58:06 -0500

    account_n_expense: balance and amount relationship, insufficient balance error

--- backend/app/models/account.rb
+++ backend/app/models/account.rb
@@ -3,4 +3,12 @@ class Account < ApplicationRecord
 
   validates :balance, :name, :number, presence: true
   validates :balance, numericality: { greater_than: 0, only_integer: true }
+
+  def deduct_balance!(amount)
+    update!(balance: balance - amount)
+  end
+
+  def add_balance(amount)
+    update(balance: balance + amount)
+  end
 end
--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,6 +1,28 @@
 class Expense < ApplicationRecord
+  before_save :update_account_balance!
+
   belongs_to :account
 
   validates :amount, :date, :description, presence: true
   validates :amount, numericality: { greater_than: 0, only_integer: true }
+
+  private
+
+  def update_account_balance!
+    return unless amount_changed? || account_id_changed?
+
+    previous_amount = amount_changed? ? changes['amount'].first : amount
+
+    if account_id_changed?
+      account.deduct_balance!(amount)
+
+      previous_account = Account.find_by_id changes['account_id'].first
+      previous_account.add_balance(previous_amount) if previous_account.present?
+    else
+      account.deduct_balance!(amount-previous_amount)
+    end
+  rescue ActiveRecord::RecordInvalid
+    errors.add(:amount, "The account balance is insufficient: #{account.balance_in_database}")
+    raise ActiveRecord::RecordInvalid.new(self)
+  end
 end
